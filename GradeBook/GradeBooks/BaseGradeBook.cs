ac5310cbe365ab2e110b585a964121bb23f787dd
Clay Hall <wclayhall@comcast.net>
2019-07-10 12:28:24 -0400

    weight

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,14 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+
+        public BaseGradeBook(string name, bool weighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = weighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +110,23 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int weight = 0;
+            if (IsWeighted && studentType != StudentType.Standard)
+                weight = 1;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + weight;
                 case 'B':
-                    return 3;
+                    return 3 + weight;
                 case 'C':
-                    return 2;
+                    return 2 + weight;
                 case 'D':
-                    return 1;
+                    return 1 + weight;
                 case 'F':
-                    return 0;
+                    return 0 + weight;
             }
-            return 0;
+            return 0 + weight;
         }
 
         public virtual void CalculateStatistics()
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool weighted) : base(name, weighted)
         {
             this.Type = GradeBookType.Ranked;
         }
